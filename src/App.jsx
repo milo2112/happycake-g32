07e904be5584f082afb18489ec136ffc68c6d07c
milo2112 <mjrp.ros@gmail.com>
2023-07-08 15:48:30 -0400

    Uploading React App for HappyCake challenge Review

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from "./views/Home"
+import Contact from "./views/Contact"
+//import NotFound from "./views/NotFound"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contactform" element={<Contact />} />
+          {/* <Route path="*" element={<Notfound />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+
   );
 }
 
